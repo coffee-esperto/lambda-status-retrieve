495ffceb89f9fb4f17dbd9ab091ef77c1f3baac8
ldubiela <ldubiela@uolinc.com>
2019-10-11 11:48:35 -0300

    Adicionando resposta no formato para o slack

--- handler.js
+++ handler.js
@@ -32,11 +32,36 @@ async function getStatusFromLocations(locations) {
   return results;
 }
 
+function isSlack(event) {
+  return event.hasOwnProperty('formparams');
+}
+
+function buildResponseSlack(results) {
+  let message = {
+    attachments: [],
+  }
+  
+  results.forEach((item) => {
+    message.attachments.push({
+      title: item.location,
+      text: (item.hasCoffee ? "tem café! :coffee:" : "acabou o café! :x:"),
+      color: (item.hasCoffee ? 'good' : 'danger'),
+      ts: Math.floor(item.createdAt/1000)
+    });
+  });
+  
+  return message;
+}
+
 module.exports.statusRetrive = async (event) => {
   const locations = ["Sala 1006", "Sala 1007"];
   
   let results = await getStatusFromLocations(locations);
-
+  
+  if (isSlack(event)) {
+    return buildResponseSlack(results);
+  }
+  
   return {
     statusCode: 200,
     body: {
